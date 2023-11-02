fba9458110b6fc9e33a889847492bac227c5ea4d
webjsmaster <web.js.master@gmail.com>
2023-11-02 22:57:16 +0300

    fix: Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,10 +2,7 @@ import { FC, useEffect } from 'react'
 import Layout from '../../components/layout/Layout.tsx'
 import LoaderPage from '../../components/loaders/loader-page/LoaderPage.tsx'
 import { useAppActions, useAppSelector } from '../../hooks/hooks.ts'
-import localStore from 'store'
-import { LOCALSTORAGE_ITEM } from '../../utils/constants.ts'
 import UserItem from '../../components/user-item/UserItem.tsx'
-import { useNavigate } from 'react-router-dom'
 import { useGetCurrentUser } from '../../hooks/useGetCurrentUser.ts'
 
 
@@ -13,45 +10,16 @@ const Home: FC = () => {
 
     const { token, user } = useGetCurrentUser()
 
-    const navigate = useNavigate()
-
-    const getStoredState = () => {
-        if (localStore.get(LOCALSTORAGE_ITEM)) {
-            const { accessToken } = localStore.get(LOCALSTORAGE_ITEM)
-            return accessToken
-        }
-    }
 
     const { getFriends } = useAppActions()
 
-    const { isLoading, friends, error } = useAppSelector(state => state.users)
+    const { isLoading, friends } = useAppSelector(state => state.users)
 
     useEffect(() => {
         if (user) {
             getFriends({ token, id: user.id })
         }
     }, [getFriends, token, user])
-
-    // const {
-    //     data: friends,
-    //     isLoading,
-    //     isSuccess,
-    //     error,
-    //     isError
-    // } = useGetFriendsQuery({ token: getStoredState(), id: user.id }, {
-    //     skip: !user.id
-    // })
-    //
-    // function isFetchBaseQueryError(error: unknown): error is IBaseError {
-    //     return typeof error === 'object' && error != null && 'status' in error
-    // }
-    //
-    //
-    // useEffect(() => {
-    //     if (error) {
-    //         navigate(LOGIN_ROUTE)
-    //     }
-    // }, [error, navigate])
 
 
     return (
